f4445bdfcb05694f1ee7768617f31f153fe9cc2b
BoykoAlex <aboyko@pivotal.io>
2020-08-27 19:48:18 -0400

    Initial commit

---
+++ terminal-client.js
@@ -0,0 +1,115 @@
+function resizeTerminal(terminal, fitAddon, ws, id) {
+    if (terminal.element.clientWidth > 0 && terminal.element.clientHeight > 0) {
+        fitAddon.fit();
+        ws.send(JSON.stringify({
+            type: 'size',
+            id: id,
+            size: {
+                cols: terminal.cols,
+                rows: terminal.rows
+            }
+        }));
+    }
+}
+
+function ping(ws) {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+        ws.send(JSON.stringify({type: "ping"}));
+        setTimeout(ping, 50000);
+    }
+}
+
+function startTerminal(elementId, id, wsUrl, cmd, cwd, theme) {
+
+    const options = {
+        cursorBlink: false,
+        theme: theme,
+    };
+
+    if (theme.fontFamily) {
+        options.fontFamily = theme.fontFamily;
+    }
+
+    if (theme.fontSize) {
+        options.fontSize = theme.fontSize;
+    }
+
+    const terminal = new Terminal(options);
+
+    const fitAddon = new FitAddon.FitAddon();
+
+    const terminalParent = document.getElementById(elementId);
+
+    terminal.loadAddon(fitAddon);
+
+    terminal.open(terminalParent);
+
+    const ws = new WebSocket(wsUrl);
+
+    ws.onopen = function() {
+        // Web Socket is connected, send data using send()
+        // ws.send("Message to send");
+        ws.send(JSON.stringify({
+            type: 'init',
+            id: id,
+            cmd: cmd,
+            cwd: cwd
+        }));
+
+        // Setup periodic ping messages
+        ping(ws);
+
+        // Send initial size of the terminal for the pty process to adjust
+        // Unless the size of the parent element is 0
+        resizeTerminal(terminal, fitAddon, ws, id);
+    };
+
+    ws.onmessage = function (evt) {
+        const message = JSON.parse(evt.data);
+        if (message.id === id) {
+            terminal.write(message.data);
+        } else {
+            console.warning("Client id " + id + " received message for id " + message.id);
+        }
+    };
+
+    ws.onclose = function(evt) {
+        terminal.write("Closed");
+    };
+
+    let previousData = '';
+    const is_eclipse_old_win_browser = typeof window.navigator.userAgent === 'string'
+        && window.navigator.userAgent.indexOf("Windows NT 6.2") >= 0;
+    const is_eclipse_old_mac_browser = typeof window.navigator.userAgent === 'string'
+        && window.navigator.userAgent.indexOf("Safari/522.0") >= 0
+    terminal.onData(function(data) {
+        if (is_eclipse_old_win_browser || is_eclipse_old_mac_browser) {
+            if (data.length === 1 && data === previousData) {
+                // Workaround double input on eclipse browser on mac
+                // skip - don't send the message. Let the next message however
+                previousData = '';
+                return;
+            } else if (data === '\b' && is_eclipse_old_mac_browser) {
+                // Ignore backspace char appearing on mac in eclipse browser. Otherwise pressing delete results in Del and Backspace. Thus ignore Backspace
+                previousData = data;
+                return;
+            } else {
+                previousData = data;
+            }
+        }
+
+        ws.send(JSON.stringify({
+            type: 'data',
+            id: id,
+            data: data
+        }));
+
+    });
+
+    new ResizeSensor(terminalParent, _.throttle(function() {
+        if (ws.readyState === WebSocket.OPEN) {
+            resizeTerminal(terminal, fitAddon, ws, id);
+        }
+    }, 250));
+
+}
---
+++ terminal-server.ts
@@ -0,0 +1,354 @@
+import * as express from 'express';
+import * as WebSocket from 'ws';
+import * as path from 'path';
+import * as pty from 'node-pty';
+import {IPty} from "node-pty";
+
+const INVALID_TIMER = -1;
+const MAX_PAGE = 3;
+const PREFERRED_CHECK_PERIOD = 5000;
+
+
+interface ColorTheme {
+    /** The default foreground color */
+    foreground?: string;
+    /** The default background color */
+    background?: string;
+    /** The cursor color */
+    cursor?: string;
+    /** The accent color of the cursor (fg color for a block cursor) */
+    cursorAccent?: string;
+    /** The selection background color (can be transparent) */
+    selection?: string;
+}
+
+interface Theme extends ColorTheme {
+    fontFamily?: string;
+    fontSize?: number;
+}
+
+interface Size {
+    rows: number;
+    cols: number;
+}
+
+interface Message {
+    type: 'ping' | 'size' | 'data' | 'init';
+    id: string;
+    data: string;
+    cmd: string[];
+    cwd: string;
+    size: Size;
+}
+
+interface PtyProperties {
+    shutdown: 'NEVER' | 'IMMEDIATELY' | 'DELAY';
+    shutdownDelay: number;
+}
+
+interface ShutdownProperties {
+    on: boolean;
+    delay: number;
+}
+
+class PtyProcessInfo {
+
+    id: string;
+    pty: IPty;
+    buffer: string[];
+    sockets: WebSocket[];
+    terminationTimer: number;
+
+    constructor(pty: IPty, id: string) {
+        this.id = id;
+        this.pty = pty;
+        this.sockets = [];
+        this.buffer = [];
+        this.terminationTimer = INVALID_TIMER;
+    }
+
+    isOverBufferSize(): boolean {
+        const max = this.pty.cols * this.pty.rows * MAX_PAGE;
+        const totalLength = this.buffer.length === 0 ? 0 : this.buffer.map(s => s.length).reduce((s, c) => s + c);
+        return totalLength > max;
+    }
+}
+
+class PtyProcessManager {
+
+    private processes: Map<string, PtyProcessInfo>;
+    private options: PtyProperties;
+
+    constructor(options: PtyProperties) {
+        this.processes = new Map<string, PtyProcessInfo>();
+        this.options = options || {
+            on: false,
+            delay: 60
+        };
+    }
+
+    private broadcastData(processInfo: PtyProcessInfo, data: string) {
+        const msg = {
+          type: 'data',
+          id: processInfo.id,
+          data: data
+        };
+
+        processInfo.sockets.forEach(s => {
+            s.send(JSON.stringify(msg));
+        });
+
+        if (processInfo.isOverBufferSize()) {
+            processInfo.buffer.shift();
+        }
+        processInfo.buffer.push(msg.data);
+
+    }
+
+    private terminatePty(ptyProcessInfo: PtyProcessInfo): boolean {
+        if (ptyProcessInfo && ptyProcessInfo.sockets.length === 0) {
+            console.info("Terminating pty process for id=" + ptyProcessInfo.id);
+            ptyProcessInfo.pty.kill();
+            this.processes.delete(ptyProcessInfo.id);
+            return true;
+        }
+        return false;
+    }
+
+    private connectSocket(ws: WebSocket, processInfo: PtyProcessInfo) {
+        if (processInfo.terminationTimer !== INVALID_TIMER) {
+            clearTimeout(processInfo.terminationTimer);
+            processInfo.terminationTimer = INVALID_TIMER;
+        }
+        processInfo.sockets.push(ws);
+    }
+
+    public isEmpty() {
+        return this.processes.size === 0;
+    }
+
+    public destroy() {
+        this.processes.forEach(processInfo => this.terminatePty(processInfo));
+    }
+
+    create(id: string, cmd: string[], cwd: string): PtyProcessInfo {
+        let processInfo = this.processes.get(id);
+        if (!cmd || !cmd.length) {
+            throw new Error('Shell command is empty!');
+        }
+        const shell = cmd.shift() || 'bash' ;
+        if (!processInfo) {
+            const ptyProcess = pty.spawn(shell, cmd, {
+                name: 'xterm-color',
+                cols: 120,
+                rows: 30,
+                cwd: cwd || process.cwd(),
+                env: <any> process.env
+            });
+            const newProcessInfo = new PtyProcessInfo(ptyProcess, id);
+            this.processes.set(id, newProcessInfo);
+
+            ptyProcess.onData(data => this.broadcastData(newProcessInfo, data));
+            processInfo = newProcessInfo;
+        }
+        return processInfo;
+    }
+
+    public createOrConnect(ws: WebSocket, id: string, cmd: string[], cwd: string): PtyProcessInfo {
+        let processInfo = <PtyProcessInfo> this.processes.get(id);
+        if (!processInfo) {
+            processInfo = this.create(id, cmd, cwd);
+            this.processes.set(id, processInfo);
+        }
+        this.connectSocket(ws, processInfo);
+        return processInfo;
+    }
+
+    public disconnectSocket(ws: WebSocket) {
+        this.processes.forEach(ptyProcessInfo => {
+            const index = ptyProcessInfo.sockets.indexOf(ws);
+            if (index >= 0) {
+                ptyProcessInfo.sockets.splice(index, 1);
+                switch (this.options.shutdown) {
+                    case 'IMMEDIATELY':
+                        this.terminatePty(ptyProcessInfo);
+                        break;
+                    case 'DELAY':
+                        (<any>ptyProcessInfo).terminationTimer = setTimeout(() => this.terminatePty(ptyProcessInfo), this.options.shutdownDelay * 1000);
+                        break;
+                }
+            }
+        });
+    }
+
+    public get(id: string) {
+        return this.processes.get(id);
+    }
+
+}
+
+
+const ARG_PORT_PREFIX = '--server.port=';
+const ARG_PTY_SHUTDOWN = '--terminal.pty.shutdown=';
+const ARG_PTY_SHUTDOWN_DELAY = '--terminal.pty.shutdown-delay=';
+const ARG_APP_AUTO_SHUTDOWN = '--terminal.auto-shutdown.on=';
+const ARG_APP_AUTO_SHUTDOWN_DELAY = '--terminal.auto-shutdown.delay=';
+
+const ptyProperties: PtyProperties = {
+    shutdown: 'NEVER',
+    shutdownDelay: 60
+};
+
+const shutdownProperties: ShutdownProperties = {
+    on: false,
+    delay: 60
+};
+
+let cliPort = '';
+
+for (let i = 0; i < process.argv.length; i++) {
+    const value = process.argv[i];
+    if (value.startsWith(ARG_PORT_PREFIX)) {
+        cliPort = value.substr(ARG_PORT_PREFIX.length);
+    } else if (value.startsWith(ARG_PTY_SHUTDOWN)) {
+        (<any>ptyProperties).shutdown = value.substr(ARG_PTY_SHUTDOWN.length).toUpperCase();
+    } else if (value.startsWith(ARG_PTY_SHUTDOWN_DELAY)) {
+        ptyProperties.shutdownDelay = parseInt(value.substr(ARG_PTY_SHUTDOWN_DELAY.length));
+    } else if (value.startsWith(ARG_APP_AUTO_SHUTDOWN)) {
+        shutdownProperties.on = 'true' === value.substr(ARG_APP_AUTO_SHUTDOWN.length).toLowerCase();
+    } else if (value.startsWith(ARG_APP_AUTO_SHUTDOWN_DELAY)) {
+        shutdownProperties.delay = parseInt(value.substr(ARG_APP_AUTO_SHUTDOWN_DELAY.length));
+    }
+}
+
+console.info('shutdown props: ' + JSON.stringify(shutdownProperties, null, 2));
+console.info('pty props: ' + JSON.stringify(ptyProperties, null, 2));
+
+const app = express();
+
+const PORT = cliPort || process.env.TERMINAL_PORT || 3001;
+
+//initialize a simple http server
+const server = app.listen(PORT, function() { console.log(`Express server currently running on port ${PORT}`); });
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+const ptyProcessManager = new PtyProcessManager(ptyProperties);
+
+wss.on('connection', (ws: WebSocket) => {
+
+    ws.on('message', (message: string) => {
+        const msg: Message = JSON.parse(message);
+        let processInfo: PtyProcessInfo | undefined;
+        switch (msg.type) {
+            case 'init':
+                processInfo = ptyProcessManager.createOrConnect(ws, msg.id, msg.cmd, msg.cwd);
+                ws.send(JSON.stringify({
+                    type: 'data',
+                    id: msg.id,
+                    data: processInfo.buffer.join('')
+                }));
+                break;
+            case 'data':
+                processInfo = ptyProcessManager.get(msg.id);
+                if (processInfo) {
+                    processInfo.pty.write(msg.data);
+                }
+                break;
+            case 'size':
+                processInfo = ptyProcessManager.get(msg.id);
+                if (processInfo) {
+                    processInfo.pty.resize(msg.size.cols, msg.size.rows);
+                }
+                break;
+            default:
+            // Else it is a ping message with is just empty object {}
+            // Sent periodically from the client to keep the WS opened
+        }
+    });
+
+    ws.on('close', () => {
+        ptyProcessManager.disconnectSocket(ws);
+    });
+
+    ws.on('error', () => {
+        ptyProcessManager.disconnectSocket(ws);
+    });
+
+});
+
+function stopApp() {
+    ptyProcessManager.destroy();
+    server.close();
+    process.exit();
+}
+
+if (shutdownProperties.on) {
+    let timeMark = new Date().getTime();
+    const delay = 1000 * shutdownProperties.delay;
+    const period = delay * 1000 < PREFERRED_CHECK_PERIOD ? 1000 : PREFERRED_CHECK_PERIOD;
+    setInterval(() => {
+        const current = new Date().getTime();
+        if (ptyProcessManager.isEmpty()) {
+            if (current - timeMark > delay ) {
+                console.info("Auto shutting down due to no activity...");
+                stopApp();
+            }
+        } else {
+            timeMark = current;
+        }
+    }, period);
+}
+
+app.set('views', path.join(__dirname, '.'));
+app.set('view engine', 'pug');
+
+app.use('/xterm.css', express.static(path.join(__dirname,
+    './node_modules/xterm/css/xterm.css')));
+app.use('/xterm.js', express.static(path.join(__dirname,
+    './node_modules/xterm/lib/xterm.js')));
+app.use('/xterm-addon-fit.js', express.static(path.join(__dirname,
+    './node_modules/xterm-addon-fit/lib/xterm-addon-fit.js')));
+app.use('/ResizeSensor.js', express.static(path.join(__dirname,
+    './node_modules/css-element-queries/src/ResizeSensor.js')));
+app.use('/lodash.js', express.static(path.join(__dirname,
+    './node_modules/lodash/lodash.js')));
+
+app.use('/terminal-client.js', express.static(path.join(__dirname, './terminal-client.js')));
+
+app.get('/', (req, res) => {
+   res.send("Xterm is UP");
+});
+
+app.post('/shutdown', (req, res) => {
+    res.status(200).send();
+    stopApp();
+});
+
+app.get('/terminal/:id', (req, res) => {
+
+    const options: any = {
+        id: req.params.id,
+        cmd: (<string>req.query.cmd || (process.platform.startsWith('win') ? 'cmd.exe' : 'bash')).split(/\s+/),
+        cwd: req.query.cwd,
+        theme: {
+            background: req.query.bg,
+            foreground: req.query.fg,
+            selection: req.query.selection,
+            cursor: req.query.cursor,
+            cursorAccent: req.query.cursorAccent,
+            fontFamily: req.query.fontFamily,
+        }
+    };
+
+
+    if (req.query.fontSize) {
+        options.theme.fontSize = parseInt(<string>req.query.fontSize);
+    }
+
+    res.render('terminal', {
+        options: JSON.stringify(options)
+    });
+});
+
